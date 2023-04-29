bc1bf958e87dafdf66998a2eae1cabe699e09f3b
ptonini <pedro.tonini@hotmail.com>
2023-04-29 19:11:22 -0300

    chore: added project files

---
+++ src/main.js
@@ -0,0 +1,26 @@
+require('dotenv-expand').expand(require('dotenv').config())
+const core = require('@actions/core');
+const {context} = require('@actions/github')
+const {Octokit} = require("@octokit/rest")
+
+async function main() {
+    const inputLabels = core.getInput('labels').split('\n').filter((x) => x !== '');
+    const octokit = new Octokit({auth: core.getInput('github_token')});
+    const {owner, repo} = context.repo;
+    const pull_number = context.payload.number
+    const {data: {labels: pullLabels}} = await octokit.rest.pulls.get({owner, repo, pull_number})
+    for (const inputLabel of inputLabels) {
+        let found = false
+        for (const l of pullLabels) {
+            const [name, value] = l.name.split(': ')
+            if (name === inputLabel) {
+                found = true
+                core.setOutput(name, value ?? true)
+                core.exportVariable(name.toUpperCase(), value ?? true)
+           }
+        }
+        found ? core.debug(`found label: ${inputLabel}`) : core.setFailed(`label ${inputLabel} not found`)
+    }
+}
+
+main().catch(err => core.setFailed(err));
