9cd1d1337feb854b08b2a22e6df42a361870704c
ptonini <pedro.tonini@hotmail.com>
2023-04-30 18:41:57 -0300

    chore: added separator input

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const {Octokit} = require("@octokit/rest")
 
 async function main() {
     const inputLabels = core.getInput('labels').split('\n').filter((x) => x !== '');
+    const separator = core.getInput('separator')
     const octokit = new Octokit({auth: core.getInput('github_token')});
     const {owner, repo} = context.repo;
     const pull_number = context.payload.number
@@ -12,11 +13,11 @@ async function main() {
     for (const inputLabel of inputLabels) {
         let found = false
         for (const l of pullLabels) {
-            const [name, value] = l.name.split(': ')
+            const [name, value] = l.name.split(separator)
             if (name === inputLabel) {
                 found = true
-                core.setOutput(name, value ?? true)
-                core.exportVariable(name.toUpperCase(), value ?? true)
+                core.setOutput(name, value?.trim() ?? true)
+                core.exportVariable(name.toUpperCase(), value?.trim() ?? true)
            }
         }
         found ? core.debug(`found label: ${inputLabel}`) : core.setFailed(`label ${inputLabel} not found`)
